d251839946616601bba0a3a6422a3cdb0581a378
yxxhero <yxxhero@example.com>
2019-06-13 22:20:59 +0800

    bugfix

--- main.py
+++ main.py
@@ -11,4 +11,8 @@ requests_timeout = 5
 if __name__ == "__main__":
     logger.info("Start get url {}".format(nginx_doc_url))
     nginx_module_page_html = requests.get(nginx_doc_url, timeout=requests_timeout).text
-    print(get_nginx_modules(nginx_module_page_html))
+    nginx_module_origin_info = get_nginx_modules(nginx_module_page_html)
+    for item in nginx_module_origin_info:
+        module_name = item.split("/")[-1].split(".")[0]
+        if module_name.startswith("ngx_"):
+            print(module_name)
