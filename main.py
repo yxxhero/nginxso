f8d74a9810749888505d07cf02602b218603adc3
yxxhero <yxxhero@example.com>
2019-06-16 20:53:57 +0800

    more

--- main.py
+++ main.py
@@ -4,20 +4,25 @@ import time
 
 from utils.log_helper import logger
 from utils.nginx_module_menu_helper import get_modules_menu
-from utils.nginx_module_page_helper import get_nginx_modules
+from utils.nginx_module_page_helper import get_nginx_modules, get_nginx_module_item, get_nginx_module_variable_info, get_modules_variables
 
 nginx_doc_url = "http://nginx.org/en/docs/"
 requests_timeout = 5
-retain_keyword = ["summary", "issues", "example", "directives", "variables", "commands", 'data', 'compatibility', 'properties']
+retain_keyword = ["arguments", "summary", "issues", "example", "directives", "compatibility", "definitions", "protocol", "variables", "commands", 'data', 'compatibility', 'properties']
 
 
 if __name__ == "__main__":
     logger.info("Start get url {}".format(nginx_doc_url))
-    nginx_module_page_html = requests.get(nginx_doc_url, timeout=requests_timeout).text
+    nginx_module_page_html = requests.get(
+        nginx_doc_url, timeout=requests_timeout).text
     nginx_module_origin_info = get_nginx_modules(nginx_module_page_html)
     for item in nginx_module_origin_info:
         module_name = item.split("/")[-1].split(".")[0]
-        if module_name.startswith("ngx_"):
-            print(get_modules_menu(nginx_doc_url + item))
-            time.sleep(3)
-
+        if module_name.startswith("ngx_") and "http_api" not in module_name:
+            # print(get_modules_menu(nginx_doc_url + item))
+            # print(get_nginx_module_item(nginx_doc_url + item))
+            variable_info = get_nginx_module_variable_info(nginx_doc_url + item)
+            if variable_info:
+                print(item)
+                print(get_modules_variables(variable_info))
+            time.sleep(1)
--- utils/nginx_module_page_helper.py
+++ utils/nginx_module_page_helper.py
@@ -7,3 +7,45 @@ def get_nginx_modules(html):
     nginx_module_location = d("center").filter(lambda i, this: pq(
         this).children('h4').text() == 'Modules reference')
     return [item.attr("href") for item in nginx_module_location.nextAll("ul").find("a").items()]
+
+
+def get_nginx_module_item(html):
+
+    d = pq(html)
+    nginx_module_item = d("a").filter(
+        lambda i, this: pq(this).attr("name") is not None)
+    return nginx_module_item
+
+
+def get_nginx_module_variable_info(html):
+
+    d = pq(html)
+    nginx_module_variable_info = d("a").filter(
+        lambda i, this: pq(this).attr("name") == "variables")
+    return nginx_module_variable_info
+
+
+def get_variable_key_value(dl_html):
+    vars_list = []
+    dt_list = list(dl_html.children("dt").items())
+    dd_list = list(dl_html.children("dd").items())
+    for i in range(len(dt_list)):
+        vars_list.append({
+            "variable_name": dt_list[i].text(),
+            "variable_desc": dd_list[i].text()
+        })
+    return vars_list
+
+
+def get_modules_variables(vars_location):
+    vars_info = {
+        "summary": ""
+    }
+    for item in vars_location.nextAll().items():
+        if item.is_("center"):
+            continue
+        elif item.is_("p") and item.text():
+            vars_info["summary"] += item.text()
+        elif item.is_("dl"):
+            vars_info["variables"] = get_variable_key_value(item)
+    return vars_info
